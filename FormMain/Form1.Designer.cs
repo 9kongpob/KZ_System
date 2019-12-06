dacbb3cb0757fb333c992bd34a7d0ce4263e18aa
9kongpob <k.kunchanuan@hotmail.com>
2019-12-06 18:57:14 +0700

    Basic Add FormMain Dashboard

--- FormMain/Form1.Designer.cs
+++ FormMain/Form1.Designer.cs
@@ -29,17 +29,28 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.MenuTab = new System.Windows.Forms.Panel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.MenuTab.SuspendLayout();
             this.SuspendLayout();
             // 
-            // panel1
+            // MenuTab
             // 
-            this.panel1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(328, 753);
-            this.panel1.TabIndex = 0;
+            this.MenuTab.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.MenuTab.Controls.Add(this.flowLayoutPanel1);
+            this.MenuTab.Dock = System.Windows.Forms.DockStyle.Left;
+            this.MenuTab.Location = new System.Drawing.Point(0, 0);
+            this.MenuTab.Name = "MenuTab";
+            this.MenuTab.Size = new System.Drawing.Size(328, 753);
+            this.MenuTab.TabIndex = 0;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(8, 8);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(312, 56);
+            this.flowLayoutPanel1.TabIndex = 0;
             // 
             // FormMain
             // 
@@ -47,7 +58,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.ClientSize = new System.Drawing.Size(1482, 753);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.MenuTab);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
@@ -55,13 +66,15 @@
             this.Name = "FormMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "KZ System";
+            this.MenuTab.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel MenuTab;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
     }
 }
 
